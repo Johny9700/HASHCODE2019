c855e60e57163ccae177069b087ddd77c74a3c63
Johny <j.dudziak97@gmail.com>
2019-02-28 19:53:19 +0100

    Add photo

--- src/photo.cpp
+++ src/photo.cpp
@@ -1,9 +1,25 @@
 #include "photo.h"
 
-photo::photo()
+
+Photo::Photo(ifstream& input)
 {
+    char c;
+    input >> c;
+     if(c == 'V'){
+         horizontal = false;
+     } else{
+         horizontal = true;
+     }
+
+    int number_of_tags;
+    input >> number_of_tags;
+    for(int i = 0; i < number_of_tags; i++){
+        string temp;
+        input >> temp;
+        mapa.insert(temp);
+    }
 }
 
-photo::~photo()
+Photo::~Photo()
 {
 }
